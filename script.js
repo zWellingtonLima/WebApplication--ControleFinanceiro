39a7d365311c3ffc90b13a0ff1cfff048734e7d2
Wellington Lima <0wellingtonlima1@gmail.com>
2022-08-26 20:16:34 +0100

    Código completo e funcional, necessário apenas refatorar para melhorias.

--- script.js
+++ script.js
@@ -1,45 +1,96 @@
-const transactionsUl = document.querySelector('#transactions');
-const incomeDisplay = document.querySelector('#money-plus');
-const expenseDisplay = document.querySelector('#money-minus');
-const balanceDisplay = document.querySelector('#balance');
+const transactionsUl = document.querySelector("#transactions");
+const incomeDisplay = document.querySelector("#money-plus");
+const expenseDisplay = document.querySelector("#money-minus");
+const balanceDisplay = document.querySelector("#balance");
+const form = document.querySelector("#form");
+const inputTransactionName = document.querySelector("#text");
+const inputTransactionAmount = document.querySelector("#amount");
 
-const dummyTransactions = [
-  { id: 1, name: 'Bolo de brigadeiro', amount: -20},
-  { id: 2, name: 'Salário', amount: 300},
-  { id: 3, name: 'Torta de frango', amount: -10},
-  { id: 4, name: 'Violão', amount: 150}
-]
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+let transactions =
+  localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
-const addTransactionIntoDom = transaction => {
-  const operator = transaction.amount < 0 ? '-' : '+';
-  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+const removeTransaction = (ID) => {
+  transactions = transactions.filter((transaction) => transaction.id !== ID);
+  updateLocalStorage();
+  init();
+};
+
+const addTransactionIntoDom = (transaction) => {
+  const operator = transaction.amount < 0 ? "-" : "+";
+  const CSSClass = transaction.amount < 0 ? "minus" : "plus";
   const amountWithoutOperator = Math.abs(transaction.amount);
-  const li = document.createElement('li');
+  const li = document.createElement("li");
 
   li.classList.add(CSSClass);
-  li.innerHTML = `${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>`;
+  li.innerHTML = `${transaction.name} 
+    <span>${operator} R$ ${amountWithoutOperator}</span>
+    <button class="delete-btn" onClick="removeTransaction(${transaction.id})">
+    x
+    </button>
+  `;
 
-  transactionsUl.append(li);
-} 
+  transactionsUl.prepend(li);
+};
 
 const updateBalanceValues = () => {
-  const transactionsAmounts = dummyTransactions
-    .map(transaction => transaction.amount);
+  const transactionsAmounts = transactions.map(
+    (transaction) => transaction.amount
+  );
   const total = transactionsAmounts
     .reduce((accumulator, transaction) => accumulator + transaction, 0)
     .toFixed(2);
   const income = transactionsAmounts
-    .filter(receita => receita > 0)
+    .filter((receita) => receita > 0)
     .reduce((accumulator, value) => accumulator + value, 0)
     .toFixed(2);
-  const expense = transactionsAmounts
-    .filter(despesa => despesa < 0)
-    .reduce((accumulator, value) => accumulator + value, 0)
-    .toFixed(2);
-}
+  const expense = Math.abs(
+    transactionsAmounts
+      .filter((despesa) => despesa < 0)
+      .reduce((accumulator, value) => accumulator + value, 0)
+  ).toFixed(2);
+
+  balanceDisplay.textContent = `R$ ${total}`;
+  incomeDisplay.textContent = `R$ ${income}`;
+  expenseDisplay.textContent = `R$ ${expense}`;
+};
 
 const init = () => {
-  dummyTransactions.forEach(addTransactionIntoDom);
+  transactionsUl.innerHTML = "";
+  transactions.forEach(addTransactionIntoDom);
   updateBalanceValues();
-}
-init()
+};
+init();
+
+const updateLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+};
+
+const generateID = () => Math.round(Math.random() * 1000);
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const transactionName = inputTransactionName.value.trim();
+  const transactionAmount = inputTransactionAmount.value.trim();
+
+  if (transactionAmount === "" || transactionName === "") {
+    alert("Por favor, preencha ambos os espaços para validar a sua transação.");
+    return;
+  }
+
+  const transaction = {
+    id: generateID(),
+    name: transactionName,
+    amount: Number(transactionAmount),
+  };
+
+  transactions.push(transaction);
+  init();
+  updateLocalStorage();
+
+  inputTransactionName.value = "";
+  inputTransactionAmount.value = "";
+});
